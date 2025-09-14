98b7051353c7fc1bc3e22ff2c9743893e618af3b
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-09-14 13:15:21 +0500

    connecting Clerk with Mongo DB Database

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -28,7 +28,6 @@ const HeroSection = () => {
           transition={{ delay: 0.3, duration: 0.6 }}
         >
           <Link
-            // href="/start"
             href={isSignedIn ? "/start" : "/sign-in"}
             className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-3 rounded-md text-base font-medium transition"
           >
